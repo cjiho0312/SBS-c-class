8380d11351dea08733aa1c98a2d79a33944254b9
cjiho0312 <cjiho0312@naver.com>
2025-05-20 15:32:01 +0900

    2025.5.20 프로젝트 생성

---
+++ program/program/source.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void main()
+{
+
+#pragma region 자료형
+	// 데이터를 저장하기 위해 데이터의 형태를 정해주는 것입니다.
+
+	// 자료형은 각각의 자료형마다 크기가 정해져 있으며
+	// 자료형의 크기는 바이트 단위로 이루어집니다.
+
+	// 자료형의 경우 자료형에 따라 저장할 수 있는
+	// 종류와 범위가 결정됩니다.
+#pragma endregion
+
+
+
+#pragma region 변수
+	// 데이터를 저장할 수 있는 메모리 공간을
+	// 생성하는 것입니다.
+
+	char alphabet = 'A';
+
+	int integer = 100;
+
+	float decimal = 36.5;
+
+	// 변수의 메모리 공간은 프로그램이 실행될 때마다 바뀝니다.
+#pragma endregion
+
+}
